1edc810aac8b9016d47ad38eb25c601e7b083735
Christian Kellner <christian@kellner.me>
2023-01-02 15:57:15 +0100

    nvim: configure more telescope keyboard mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -149,6 +149,8 @@ require('telescope').load_extension('fzy_native')
 
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
+vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
 
 -- git
 require('gitsigns').setup {
