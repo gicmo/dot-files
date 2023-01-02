fa09276c2732aac111864c327e250870e3b83bb4
Christian Kellner <christian@kellner.me>
2023-01-02 16:03:16 +0100

    nvim: configure <up>/<down> for auto-completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -295,8 +295,11 @@ local luasnip = require('luasnip')
 local cmp = require 'cmp'
 cmp.setup {
   mapping = {
+    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+     ['<Down>'] = cmp.mapping.select_next_item(select_opts),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
+
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
