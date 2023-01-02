bbefd85a52bea2c15443f5686f06f65017c01f89
Christian Kellner <christian@kellner.me>
2023-01-02 15:57:40 +0100

    nvim: install and configure trouble

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,6 +33,8 @@ require('packer').startup(function(use)
     } 
   }
 
+  use 'folke/trouble.nvim'
+
   -- git 
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
@@ -152,6 +154,25 @@ vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
 
+trouble = require('trouble')
+trouble.setup()
+
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {})
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  {silent = true, noremap = true}
+)
 -- git
 require('gitsigns').setup {
   on_attach = function(bufnr)
